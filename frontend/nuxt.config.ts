9987b0bdc161ca106443848eb678857d5b453430
Emo Maat <emosbm@gmail.com>
2023-06-19 16:09:54 +0200

    Updates

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,15 +5,36 @@ export default defineNuxtConfig({
     // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-cli#clearconsole
     css: [
         "~/assets/scss/overrides.scss",
-        '@fortawesome/fontawesome-svg-core/styles.css'
+        "@/assets/scss/icons.scss",
+        "@/assets/scss/global.scss",
+        '@fortawesome/fontawesome-svg-core/styles.css',
     ],
 
     modules: [
         '@pinia/nuxt',
     ],
 
+    runtimeConfig: {
+        public: {
+            SP_CLIENT_ID: process.env.SP_CLIENT_ID,
+            DOMAIN: process.env.DOMAIN
+        }
+    },
+    build: {
+        transpile: ['@fortawesome/vue-fontawesome'],
+    },
     vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "@/node_modules/bootstrap/scss/mixins"; @import "@/node_modules/bootstrap/scss/functions"; @import "@/node_modules/bootstrap/scss/variables";'
+                }
+            }
+        },
         server: {
+            fs: {
+                allow: ["../backend/src/"]
+            },
             hmr: {
                 port: 24672,
                 protocol: 'ws',
