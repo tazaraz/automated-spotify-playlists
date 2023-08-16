8b1db06987799bf5fcd349e018e6a830742ab14a
Emo Maat <emosbm@gmail.com>
2023-08-16 15:44:23 +0200

    Added more icons

--- frontend/plugins/fontawesome.js
+++ frontend/plugins/fontawesome.js
@@ -1,13 +1,35 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import * as fas from '@fortawesome/free-solid-svg-icons'
+import * as far from '@fortawesome/free-regular-svg-icons'
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas)
+const icons = [
+    "faQuestion",
+    "faGear",
+    "faHeart",
+    "faSearch",
+    "faPlus",
+    "faCodeBranch",
+    "faAnglesLeft",
+    "faAnglesRight",
+    "faTrashCan",
+    "faWandMagic",
+    "faBars",
+    "faUser",
+    "faArrowDownWideShort",
+    "faGripVertical",
+]
+
+const FaIcons = icons.map((icon) => {
+    return [fas[icon], far[icon]]
+}).flat().filter((icon) => icon != undefined)
+
+library.add(FaIcons)
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('fa-icon', FontAwesomeIcon)
