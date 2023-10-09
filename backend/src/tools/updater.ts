29c615f1cbdb32fa68c774287eb726f690ba7ebc
Emo Maat <emosbm@gmail.com>
2023-10-09 19:02:08 +0200

    Added a database cleanup function

--- backend/src/tools/updater.ts
+++ backend/src/tools/updater.ts
@@ -12,7 +12,10 @@ export default class Updater {
     static schedule(){
         // Run the updater every hour
         setInterval(async () => Updater.updatePlaylists, 1000 * 60 * 60);
+        // Clean up the database every 4 days
+        setInterval(async () => Updater.cleanDatabase, 1000 * 60 * 60 * 24 * 4);
     }
+
     /**
      * Updates all playlists in the database
      */
@@ -52,5 +55,41 @@ export default class Updater {
                 max = Math.max(...performance);
         LOG(`${date.toISOString()}: Updating playlists performance: \n - total: ${total}ms \n - avg: ${avg}ms \n - max: ${max}ms`);
     }
+
+    /**
+     * Cleans the database from playlists which are not in Spotify anymore. This can happen if a user deletes a
+     * playlist which also has no followers. We don't want to keep these playlists in our database as this is
+     * more computing.
+     */
+    static async cleanDatabase() {
+        const db_users = await Database.getAllUsers();
+        const db_playlists = await Database.getAllPlaylists();
+
+        LOG_DEBUG(`Cleaning database...`);
+
+        // Get all user playlists from Spotify
+        const users_playlists: { [user_id: string]: Playlist[] } = {};
+        for (const user of db_users) {
+            const response = await Fetch.get<Playlist[]>(`/users/${user.id}/playlists`, { pagination: true});
+
+            if (response.status !== 200)
+                THROW_DEBUG_ERROR(`Cleaning: failed to get playlists for user_id: ${user.id}. Error: \n${response}`);
+            else
+                users_playlists[user.id] = response.data;
+        }
+
+        /** We want to remove playlists in our database which are lonely in Spotify (no followers or user) */
+        const remove: Playlist[] = [];
+        for (const playlist of db_playlists) {
+            // If the user does not have this playlist anymore
+            if (!users_playlists[playlist.user_id].some(p => p.id === playlist.id))
+                // And the playlist has no followers, delete it
+                if ((await Fetch.get(`/playlists/${playlist.id}`)).data?.followers.total == 0)
+                    remove.push(playlist);
+        }
+
+        // Remove the playlists
+        for (const playlist of remove)
+            await Database.deletePlaylist(playlist.user_id, playlist.id);
     }
 }
